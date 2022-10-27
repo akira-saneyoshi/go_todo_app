d698efc7412b0faab6ac0b9933c41a6563f1f9fe
saneyoshi <saneyoshi@collabos.com>
2022-10-27 17:11:30 +0900

    add:モックコードの自動生成

--- mux.go
+++ mux.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/budougumi0617/go_todo_app/service"
+
 	"github.com/budougumi0617/go_todo_app/clock"
 	"github.com/budougumi0617/go_todo_app/config"
 	"github.com/budougumi0617/go_todo_app/handler"
@@ -24,9 +26,14 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 		return nil, cleanup, err
 	}
 	r := store.Repository{Clocker: clock.RealClocker{}}
-	at := &handler.AddTask{DB: db, Repo: &r, Validator: v}
+	at := &handler.AddTask{
+		Service:   &service.AddTask{DB: db, Repo: &r},
+		Validator: v,
+	}
 	mux.Post("/tasks", at.ServeHTTP)
-	lt := &handler.ListTask{DB: db, Repo: &r}
+	lt := &handler.ListTask{
+		Service: &service.ListTask{DB: db, Repo: &r},
+	}
 	mux.Get("/tasks", lt.ServeHTTP)
 	return mux, cleanup, nil
 }
